a3c0a7a5002aa06871a7c707fe4c824c0a57d8f6
Lazares Sathya <lazaressathya7@gmail.com>
2026-09-09 12:38:33 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>Hello from Next.js!</h1>
+
+      <p>
+        Application deployed successfully using Docker.
+      </p>
+
+      <p>
+        Ubuntu 24.04 + Docker + GitHub Actions
+      </p>
+    </main>
+  );
+}
